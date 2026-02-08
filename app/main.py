d6ec70720ba6247d11b0645c47940c2b26cfe5cc
Ajay607 <dev.ajay8962@gmail.com>
2026-02-08 23:14:24 +0530

    feat: db connection

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.database import get_db_connection
 
 app = FastAPI(title="E-commerce Backend API")
 
@@ -9,3 +10,27 @@ def health_check():
         "status": "ok",
         "service": "ecommerce-backend"
     }
+
+
+
+@app.get("/users")
+def list_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, name, email, is_active FROM users;")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    users = []
+    for row in rows:
+        users.append({
+            "id": row[0],
+            "name": row[1],
+            "email": row[2],
+            "is_active": row[3]
+        })
+
+    return users
