ae5b609f97077a5f88724f38284db0e1ba3b8861
Ajay607 <dev.ajay8962@gmail.com>
2026-02-09 23:48:57 +0530

    feat: api for products

--- app/main.py
+++ app/main.py
@@ -34,3 +34,61 @@ def list_users():
         })
 
     return users
+
+@app.get("/products")
+def list_products():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, name, price, owner_id
+        FROM products;
+    """)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    products = []
+    for row in rows:
+        products.append({
+            "id": row[0],
+            "name": row[1],
+            "price": float(row[2]),
+            "owner_id": row[3]
+        })
+
+    return products
+
+@app.get("/products-with-owner")
+def products_with_owner():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            p.id,
+            p.name,
+            p.price,
+            u.name,
+            u.email
+        FROM products p
+        JOIN users u ON p.owner_id = u.id;
+    """)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    result = []
+    for row in rows:
+        result.append({
+            "product_id": row[0],
+            "product_name": row[1],
+            "price": float(row[2]),
+            "owner_name": row[3],
+            "owner_email": row[4]
+        })
+
+    return result
+
