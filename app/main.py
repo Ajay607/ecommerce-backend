cd0f77f1e90d86d968154fceaebbb524c1e99e95
Ajay607 <dev.ajay8962@gmail.com>
2026-02-26 09:32:01 +0530

    feat: api for produt

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.database import get_db_connection
 from app.schemas import User
 from app.schemas import Product
+from app.schemas import ProductCreate
+from app.schemas import ProductWithOwner
 
 app = FastAPI(title="E-commerce Backend API")
 
@@ -59,7 +61,7 @@ def list_products():
 
     return products
 
-@app.get("/products-with-owner")
+@app.get("/products-with-owner", response_model=list[ProductWithOwner])
 def products_with_owner():
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -91,3 +93,86 @@ def products_with_owner():
 
     return result
 
+
+@app.post("/products", response_model=Product)
+def create_product(product: ProductCreate):
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # check owner exists
+    cursor.execute(
+        "SELECT id FROM users WHERE id = %s;",
+        (product.owner_id,)
+    )
+
+    owner = cursor.fetchone()
+
+    if owner is None:
+
+        cursor.close()
+        conn.close()
+
+        return {"error": "Owner not found"}
+
+    # insert product
+
+    cursor.execute(
+        """
+        INSERT INTO products (name, price, owner_id)
+        VALUES (%s, %s, %s)
+        RETURNING id, name, price, owner_id;
+        """,
+        (product.name, product.price, product.owner_id)
+    )
+
+    new_product = cursor.fetchone()
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {
+        "id": new_product[0],
+        "name": new_product[1],
+        "price": float(new_product[2]),
+        "owner_id": new_product[3]
+    }
+
+
+@app.get("/products-with-owner", response_model=list[ProductWithOwner])
+def products_with_owner():
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            p.id,
+            p.name,
+            p.price,
+            u.name,
+            u.email
+        FROM products p
+        JOIN users u ON p.owner_id = u.id;
+    """)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    result = []
+
+    for row in rows:
+
+        result.append({
+            "product_id": row[0],
+            "product_name": row[1],
+            "price": float(row[2]),
+            "owner_name": row[3],
+            "owner_email": row[4]
+        })
+
+    return result
--- app/schemas.py
+++ app/schemas.py
@@ -12,3 +12,15 @@ class Product(BaseModel):
     name: str
     price: float
     owner_id: int
+
+class ProductCreate(BaseModel):
+    name: str
+    price: float
+    owner_id: int
+
+class ProductWithOwner(BaseModel):
+    product_id: int
+    product_name: str
+    price: float
+    owner_name: str
+    owner_email: str
